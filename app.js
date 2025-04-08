a499db1b75b7aaad1584f94b29170c12f45d4308
Shahil Ahmed <shahil@agpl.net>
2025-04-08 16:13:55 +0530

    Done with Front-end

---
+++ app.js
@@ -0,0 +1,11 @@
+// const URL = "https://catfact.ninja/facts";
+
+// const getFacts = async () => {
+//     console.log("getting data ....");
+//     let response = await fetch(URL);
+//     console.log(response);
+//     let data = await response.json();
+//     console.log(data);
+// }
+
+
